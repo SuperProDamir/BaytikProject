a7312a253c38f0e61b1439928c5d69be5ac86470
SuperProDamir <35636380+SuperProDamir@users.noreply.github.com>
2019-10-22 09:55:34 +0300

    basic scripts was added

---
+++ OurBaytikProject/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensetivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Horizontal") * mouseSensetivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Vertical") * mouseSensetivity * Time.deltaTime;
+    }
+}
